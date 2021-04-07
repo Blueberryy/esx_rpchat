77d84574847195f61cea624d640a47c5c97576dc
Blueberryy <igory.ygr200@gmail.com>
2021-04-07 12:46:55 +0500

    Create ru.lua

---
+++ locales/ru.lua
@@ -0,0 +1,11 @@
+Locales['ru'] = {
+  ['ooc_prefix'] = 'OOC | %s',
+  ['twt_help'] = 'отправить твит',
+  ['twt_prefix'] = '^0[^4Твиттер^0] (^5@%s^0)',
+  ['me_help'] = 'личное действие',
+  ['me_prefix'] = 'me | %s',
+  ['do_help'] = 'RP информация',
+  ['do_prefix'] = 'do | %s',
+  ['generic_argument_name'] = 'сообщение',
+  ['generic_argument_help'] = 'сообщение',
+}
